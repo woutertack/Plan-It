6def5f25dadce6678a2c5eb5bfa847a57df9feb5
woutertack <wouttack1@student.arteveldehs.be>
2022-12-08 14:44:20 +0100

    update

---
+++ opdracht3-wouter-tack/.eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['airbnb-base', 'airbnb-typescript/base'],
+  overrides: [
+  ],
+  parserOptions: {
+    project: './tsconfig.json'
+  },
+  rules: {
+    'linebreak-style': 0,
+  }
+}
---
+++ opdracht3-wouter-tack/src/main.ts
@@ -0,0 +1,9 @@
+import './css/style.css'
+
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+   Start project
+  </div>
+`
+
